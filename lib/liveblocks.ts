971e0cce69c770383df59341fed8eac3cf7c5207
Aditya <rawaladitya886@gmail.com>
2026-09-20 17:17:38 +0530

    Updated project files

--- lib/liveblocks.ts
+++ lib/liveblocks.ts
@@ -1,5 +1,5 @@
 import { Liveblocks } from "@liveblocks/node";
 
 export const liveblocks = new Liveblocks({
-  secret: process.env.LIVEBLOCKS_SECRET_KEY as string,
+  secret: process.env.LIVEBLOCKS_SECRET_KEY || "sk_dummy_key_to_allow_build",
 });
